47e6e8c65e721a58737fcd418305ba52830cd0c6
alensden2 <alensanjohn1999@gmail.com>
2023-08-27 15:06:38 -0300

    Page setup

---
+++ src/Pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar />
+      <h1>Header</h1>
+    </div>
+  )
+}
